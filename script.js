e44d8b8ccfd2f5c568a5ab0115d65da1d9f731dc
Fábio Henrique <122043463+fabiocodee@users.noreply.github.com>
2025-05-14 22:11:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+/*declarando variáveis*/
+let altura;
+let peso;
+let imc;
+/*CRIANDO UMA FUNÇÃO PARA CALCULO DO IMC*/
+//COMENTARIO DE LINHA
+/*COMENTARIO DE BLOCO*/
+function calcImc( ){
+    let altura= (document.getElementById("al").value)/100;//divisao por 100
+    let peso = document.getElementById("ps").value
+altura = parseFloat(altura)
+/*
+tipos de dados:
+int: numeros inteiros 
+float:de poto flutuante exemplo , exemplo da gasolina6.27
+string: texto
+boolean : verdadeiro ou falso */
+peso = parseFloat(peso)  
+
+let imc = peso / (altura * altura)
+
+window.alert ("Seu IMC é" + imc.toFixed(2))
+}
